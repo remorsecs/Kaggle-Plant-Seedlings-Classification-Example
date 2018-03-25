e11aa64705543f639edd81977d24a660ad3750e1
Maniac <remorse@csie.io>
2018-03-26 02:42:23 +0800

    Add VGG16 model

---
+++ models.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+import math
+
+
+class VGG16(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            # input shape: (batch_size, 3, 224, 224) and
+            # downsampled by a factor of 2^5 = 32 (5 times maxpooling)
+            # So features' shape is (batch_size, 7, 7, 512)
+            nn.Linear(in_features=7 * 7 * 512, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(in_features=4096, out_features=num_classes)
+        )
+
+        # initialize parameters
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                n = module.kernel_size[0] * module.kernel_size[1] * module.out_channels
+                # module.reset_parameters()
+                module.weight.data.normal_(0, math.sqrt(2. / n))
+                module.bias.data.zero_()
+            elif isinstance(module, nn.Linear):
+                # module.reset_parameters()
+                module.weight.data.normal_(0, 0.01)
+                module.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # flatten
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
